1cfdd0e80b2f8da5bf1012de73ed36be01370647
Axl Nunez <axltico12@gmail.com>
2025-12-20 02:55:07 -0500

    Added settings navigation to StartupToolbarFragment

--- app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/StartupToolbarFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/StartupToolbarFragment.kt
@@ -1,16 +1,20 @@
 package org.jellyfin.androidtv.ui.startup.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.ui.base.JellyfinTheme
+import org.jellyfin.androidtv.ui.preference.PreferencesActivity
+import org.jellyfin.androidtv.ui.preference.screen.CustomizationPreferencesScreen
 import org.jellyfin.androidtv.ui.shared.toolbar.StartupToolbar
 
 class StartupToolbarFragment : Fragment() {
@@ -28,6 +32,17 @@ class StartupToolbarFragment : Fragment() {
 							addToBackStack(null)
 							replace<ConnectHelpAlertFragment>(R.id.content_view)
 						}
+					},
+					openSettings = {
+						val intent = Intent(requireContext(), PreferencesActivity::class.java).apply {
+							putExtras(
+								bundleOf(
+									PreferencesActivity.EXTRA_SCREEN to CustomizationPreferencesScreen::class.qualifiedName,
+									PreferencesActivity.EXTRA_SCREEN_ARGS to bundleOf(),
+								)
+							)
+						}
+						startActivity(intent)
 					}
 				)
 			}
